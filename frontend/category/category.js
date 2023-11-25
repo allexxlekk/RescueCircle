fb25be496542982f9710bcb89f9430ccff38b427
ntinoskost16 <ntinos.kostos@gmail.com>
2023-11-25 21:52:10 +0200

    Create category page

--- frontend/category/category.js
+++ frontend/category/category.js
@@ -1,69 +1,62 @@
-let categoriesCount = [
-   {
-    category: 'Food',
-    itemCount: 3,
- },
- {
-    category: 'Fruit',
-    itemCount: 4,
- },
- {
-    category: 'Water',
-    itemCount: 24,
- },
-];
+  function renderCategories(categoriesCount) {
+      const container = document.querySelector('#container');
+      container.innerHTML = ''; 
 
-for (let i = 0; i < categoriesCount.length; i++) {
-    const container = document.querySelector('#container');
-    const myList = document.createElement('ul');
-    myList.classList.add('category-list');
-    container.appendChild(myList);
+      categoriesCount.forEach(categoryInfo => {
+        const myList = document.createElement('ul');
+        myList.classList.add('category-list');
+        container.appendChild(myList);
 
-    const categoryRow = document.createElement('div');
-    categoryRow.classList.add('category-row');
-    myList.appendChild(categoryRow);
+        const categoryRow = document.createElement('div');
+        categoryRow.classList.add('category-row');
+        myList.appendChild(categoryRow);
 
-    const category = document.createElement('div');
-    category.classList.add('category');
-    const itemCount = document.createElement('div');
-    itemCount.classList.add('item-count');
+        const category = document.createElement('div');
+        category.classList.add('category');
+        const itemCount = document.createElement('div');
+        itemCount.classList.add('item-count');
 
-    categoryRow.appendChild(category);
-    categoryRow.appendChild(itemCount);
+        categoryRow.appendChild(category);
+        categoryRow.appendChild(itemCount);
 
-    category.textContent = `Category: ${categoriesCount[i].category}`;
-    itemCount.textContent = `Number of items: ${categoriesCount[i].itemCount}`;
-  }
-    
-const addButton = document.querySelector('#add-button');
-
-addButton.addEventListener('click', () => {
-    let container = document.querySelector('#container');
-    let categoryAdded = document.createElement('ul');
-    categoryAdded.classList.add('category-list');
-    let categoryRow = document.createElement('div');
-    categoryRow.classList.add('category-row');
-    let category = document.createElement('div');
-    category.classList.add('category');
-    
-
-    container.appendChild(categoryAdded);
-    categoryAdded.appendChild(categoryRow);
-    categoryRow.appendChild(category);
- 
-
-    const newCategory = document.querySelector('#new-category').value;
-    
-    container.appendChild(categoryAdded);
-    if (newCategory ) {
-    categoriesCount.push({ category: newCategory});
-        for(let i = 0; i < categoriesCount.length; i++) {
-        category.textContent = `Category: ${categoriesCount[i].category} `
-    
+        category.textContent = `Category: ${categoryInfo.category}`;
+        itemCount.textContent = `Number of items: ${categoryInfo.itemCount}`;
+      });
     }
-    } else {
-    alert('Please enter valid input for category and item count.');
+
+    function fetchCategories() {
+      fetch('http://localhost:3000/categories/count')
+        .then(response => response.json())
+        .then(data => {
+          renderCategories(data); 
+        })
+        .catch(error => {
+          console.error('Error fetching categories:', error);
+        });
     }
-});
 
+    renderCategories([]); // Initial render with an empty array
 
+    const addButton = document.querySelector('#add-button');
+    addButton.addEventListener('click', () => {
+      // Assuming you have an input field for the new category
+      const newCategory = document.querySelector('#new-category').value;
+
+      // Validate input (you may want to add more thorough validation)
+      if (newCategory) {
+        // Perform your POST request to add a new category with query parameter
+        fetch(`http://localhost:3000/categories?categoryName=${encodeURIComponent(newCategory)}`, {
+          method: 'POST',
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Category added successfully:', data);
+          fetchCategories(); // Re-fetch and render categories after adding a new one
+        })
+        .catch(error => {
+          console.error('Error adding category:', error);
+        });
+      } else {
+        alert('Please enter a valid input for the new category.');
+      }
+    });
