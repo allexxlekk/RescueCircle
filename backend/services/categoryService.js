72bc98370389d852280ec3611a4d04b8f446fe6c
kogi4ever <47474645+kogi4ever@users.noreply.github.com>
2023-11-25 20:43:55 +0200

    create get category count in categoryService

--- backend/services/categoryService.js
+++ backend/services/categoryService.js
@@ -1,12 +1,16 @@
 const bcrypt = require('bcrypt');
 const dbConnection = require('../config/db');
 
+const categoryExists = async (categoryName) => {
+  const checkQuery = 'SELECT COUNT(*) AS count FROM item_category WHERE name = ?';
+  const [checkResult] = await dbConnection.promise().query(checkQuery, [categoryName]);
+  return checkResult[0].count !== 0
+}
+
 
 const addCategory = async (categoryName) => {
   try {
-      const checkQuery = 'SELECT COUNT(*) AS count FROM item_category WHERE name = ?';
-      const [checkResult] = await dbConnection.promise().query(checkQuery, [categoryName]);
-      if (checkResult[0].count === 0) {
+      if (!await categoryExists(categoryName)){ 
           const query = 'INSERT INTO item_category (name) VALUES (?)';
           await dbConnection.promise().query(query, [categoryName]);
           return true; // Category added successfully
@@ -18,6 +22,8 @@ const addCategory = async (categoryName) => {
       throw err;
   }
 };
+
+
 
 const getAllCategories = async () => {
   try {
@@ -33,7 +39,7 @@ const getAllCategories = async () => {
 const getItemsCountInCategory = async () => {
   try {
     const query = `
-    SELECT item_category.name, COUNT(item.id) AS quantity
+    SELECT item_category.name AS category , COUNT(item.id) AS itemCount
     FROM item_category
     LEFT JOIN item ON item_category.id = item.category_id
     GROUP BY item_category.name;
