685e42b095e74f48e4825d84fbabe3ebc754fe19
ntinoskost16 <ntinos.kostos@gmail.com>
2023-11-26 21:56:53 +0200

    Update items frontend page

--- frontend/category/category.js
+++ frontend/category/category.js
@@ -1,71 +1,73 @@
 
-let categoriesCount=[];
-  function renderCategories(categoriesCount) {
-      const container = document.querySelector('#container');
-      container.textContent = ''; 
+let categoriesCount = [];
+function renderCategories(categoriesCount) {
+  const container = document.querySelector('#container');
+  container.textContent = '';
 
-      categoriesCount.forEach(categoryInfo => {
-        const myList = document.createElement('ul');
-        myList.classList.add('category-list');
-        container.appendChild(myList);
+  categoriesCount.forEach(categoryInfo => {
+    const myList = document.createElement('ul');
+    myList.classList.add('category-list');
+    container.appendChild(myList);
 
-        const categoryRow = document.createElement('div');
-        categoryRow.classList.add('category-row');
-        myList.appendChild(categoryRow);
+    const categoryRow = document.createElement('div');
+    categoryRow.classList.add('category-row');
+    myList.appendChild(categoryRow);
 
-        const category = document.createElement('div');
-        category.classList.add('category');
-        const itemCount = document.createElement('div');
-        itemCount.classList.add('item-count');
+    const category = document.createElement('div');
+    category.classList.add('category');
+    const itemCount = document.createElement('div');
+    itemCount.classList.add('item-count');
 
-        categoryRow.appendChild(category);
-        categoryRow.appendChild(itemCount);
+    categoryRow.appendChild(category);
+    categoryRow.appendChild(itemCount);
 
-        category.textContent = `Category: ${categoryInfo.category}`;
-        itemCount.textContent = `Number of items: ${categoryInfo.itemCount}`;
-      });
-    }
+    category.textContent = `Category: ${categoryInfo.category}`;
+    itemCount.textContent = `Number of items: ${categoryInfo.itemCount}`;
+  });
+}
 
-    function fetchCategories() {
-      fetch('http://localhost:3000/categories/count')
-        .then(response => response.json())
-        .then(data => {
-          renderCategories(data); 
-        })
-        .catch(error => {
-          console.error('Error fetching categories:', error);
-        });
-    }
-// Initial render with array categoriesCount
-    renderCategories(categoriesCount); 
-
-    const addButton = document.querySelector('#add-button');
-    addButton.addEventListener('click', () => {
-      
-      const newCategory = document.querySelector('#new-category').value;
-
-      const categoryExists = categoriesCount.some(categoryInfo => categoryInfo.category === newCategory);
-
-      if (categoryExists) {
-          console.log('The category already exists.');
-      }
-      
-      else if (newCategory) {
-        //POST request to add a new category with query parameter
-        fetch(`http://localhost:3000/categories?categoryName=${encodeURIComponent(newCategory)}`, {
-          method: 'POST',
-        })
-        .then(response => response.json())
-        .then(data => {
-          console.log('Category added successfully:', data);
-          fetchCategories(categoriesCount); // Re-fetch and render categories after adding a new one
-        })
-        .catch(error => {
-          console.error('Error adding category:', error);
-        })}
-      
-      else {
-        alert('Please enter a valid input for the new category.');
-      }
+function fetchCategories() {
+  fetch('http://localhost:3000/categories/count')
+    .then(response => response.json())
+    .then(data => {
+      renderCategories(data);
+    })
+    .catch(error => {
+      console.error('Error fetching categories:', error);
     });
+}
+// Initial render with array categoriesCount
+renderCategories(categoriesCount);
+
+const addButton = document.querySelector('#add-button');
+addButton.addEventListener('click', () => {
+
+  const newCategory = document.querySelector('#new-category').value;
+
+  const categoryExists = categoriesCount.some(categoryInfo => categoryInfo.category === newCategory);
+
+  if (categoryExists) {
+    console.log('The category already exists.');
+  }
+
+  else if (newCategory) {
+    //POST request to add a new category with query parameter
+    fetch(`http://localhost:3000/categories?categoryName=${encodeURIComponent(newCategory)}`, {
+      method: 'POST',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Category added successfully:', data);
+        fetchCategories(categoriesCount); // Re-fetch and render categories after adding a new one
+      })
+      .catch(error => {
+        console.error('Error adding category:', error);
+      })
+  }
+
+  else {
+    alert('Please enter a valid input for the new category.');
+  }
+});
 fetchCategories();
+
--- frontend/item/item.js
+++ frontend/item/item.js
@@ -17,20 +17,20 @@ itemArray = [
     },
 ];
 
-const container = document.querySelector('#container');
+
 //Get item by categoryID
 function getItemById(category_id) {
     // Filter items based on category_id
     const selectedItems = itemArray.filter(item => item.category_id === category_id);
 
     if (selectedItems.length > 0) {
-        
+
         const myList = document.createElement('ul');
         myList.classList.add('list-item');
         container.appendChild(myList);
 
         selectedItems.forEach(selectedItem => {
-           
+
             const listItem = document.createElement('li');
             myList.appendChild(listItem);
 
@@ -46,17 +46,17 @@ function getItemById(category_id) {
 }
 
 
-function getItemDetailsByName(name){
+function getItemDetailsByName(name) {
     const selectedItems = itemArray.filter(item => item.name === name);
 
     if (selectedItems.length > 0) {
-        
+
         const myList = document.createElement('ul');
         myList.classList.add('list-item');
         container.appendChild(myList);
 
         selectedItems.forEach(selectedItem => {
-           
+
             const listItem = document.createElement('li');
             myList.appendChild(listItem);
 
@@ -71,5 +71,43 @@ function getItemDetailsByName(name){
     }
 }
 
-getItemDetailsByName('Bananas');
+async function getItems() {
+    try {
+        const items = await fetchItems();
 
+        if (items && items.length > 0) {
+            const container = document.querySelector('#container');
+            const myList = document.createElement('ul');
+            myList.classList.add('list-item');
+            container.appendChild(myList);
+
+            items.forEach(selectedItem => {
+                const listItem = document.createElement('li');
+                myList.appendChild(listItem);
+
+                const getItemDisplay = document.createElement('div');
+                getItemDisplay.classList.add('item-display');
+                listItem.appendChild(getItemDisplay);
+
+                getItemDisplay.textContent = `Item id: ${selectedItem.id} Name: ${selectedItem.name} Description: ${selectedItem.description} Quantity: ${selectedItem.quantity} Offer: ${selectedItem.offer_quantity} Category id: ${selectedItem.category_id}`;
+            });
+        } else {
+            console.error('No items found.');
+        }
+    } catch (error) {
+        console.error('Error fetching items:', error);
+    }
+}
+
+async function fetchItems() {
+    try {
+        const response = await fetch('http://localhost:3000/items');
+        const data = await response.json();
+        return data; // Return the data
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error; // Re-throw the error to be caught in the higher level
+    }
+}
+
+getItems();
