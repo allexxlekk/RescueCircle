a50316126f75e81cd54cd361e44e30e982e57e60
ntinoskost16 <ntinos.kostos@gmail.com>
2023-11-26 15:03:14 +0200

    Create get item details by name

--- frontend/item/item.js
+++ frontend/item/item.js
@@ -18,8 +18,8 @@ itemArray = [
 ];
 
 const container = document.querySelector('#container');
-
-function getItem(category_id) {
+//Get item by categoryID
+function getItemById(category_id) {
     // Filter items based on category_id
     const selectedItems = itemArray.filter(item => item.category_id === category_id);
 
@@ -45,4 +45,31 @@ function getItem(category_id) {
     }
 }
 
-getItem(8);
+
+function getItemDetailsByName(name){
+    const selectedItems = itemArray.filter(item => item.name === name);
+
+    if (selectedItems.length > 0) {
+        
+        const myList = document.createElement('ul');
+        myList.classList.add('list-item');
+        container.appendChild(myList);
+
+        selectedItems.forEach(selectedItem => {
+           
+            const listItem = document.createElement('li');
+            myList.appendChild(listItem);
+
+            const getItemDisplay = document.createElement('div');
+            getItemDisplay.classList.add('item-display');
+            listItem.appendChild(getItemDisplay);
+
+            getItemDisplay.textContent = `Item id: ${selectedItem.id} Name: ${selectedItem.name} Description: ${selectedItem.description} Quantity: ${selectedItem.quantity} Offer: ${selectedItem.offer_quantity} Category id: ${selectedItem.category_id}`;
+        });
+    } else {
+        console.error(`No items found with category_id = ${name}.`);
+    }
+}
+
+getItemDetailsByName('Bananas');
+
