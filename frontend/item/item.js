a811f9a22d82c60c8cf56319ba61f516aa2574b6
ntinoskost16 <ntinos.kostos@gmail.com>
2023-12-04 21:36:35 +0200

    Comment out bad code

--- frontend/item/item.js
+++ frontend/item/item.js
@@ -2,7 +2,6 @@ async function getItemByCategoryId(category_id) {
 
     // Filter items based on category_id
     const selectedItems = await fetchItemsByCategoryId(category_id);
-
     if (selectedItems && selectedItems.length > 0) {
 
         const myList = document.createElement('ul');
@@ -25,10 +24,11 @@ async function getItemByCategoryId(category_id) {
     }
 }
 
-function getItemDetailsByName(name) {
-    const selectedItems = itemArray.filter(item => item.name === name);
+async function getItemBySearch(name) {
 
-    if (selectedItems.length > 0) {
+    // Filter items based on category_id
+    const selectedItems = await fetchItemsBySearch(name);
+    if (selectedItems && selectedItems.length > 0) {
 
         const myList = document.createElement('ul');
         myList.classList.add('list-item');
@@ -43,12 +43,36 @@ function getItemDetailsByName(name) {
             getItemDisplay.classList.add('item-display');
             listItem.appendChild(getItemDisplay);
 
-            getItemDisplay.textContent = `Item id: ${selectedItem.id} Name: ${selectedItem.name} Description: ${selectedItem.description} Quantity: ${selectedItem.quantity} Offer: ${selectedItem.offer_quantity} Category id: ${selectedItem.category_id}`;
+            getItemDisplay.textContent = `Item id: ${selectedItem.id} Name: ${selectedItem.name} Description: ${selectedItem.description} Quantity: ${selectedItem.quantity} Offer: ${selectedItem.quantity} Category id: ${selectedItem.category_name}`;
         });
     } else {
-        console.error(`No items found with category_id = ${name}.`);
+        console.error(`No items found with Search  = ${name}.`);
     }
 }
+// function getItemDetailsByName(name) {
+//     const selectedItems = itemArray.filter(item => item.name === name);
+
+//     if (selectedItems.length > 0) {
+
+//         const myList = document.createElement('ul');
+//         myList.classList.add('list-item');
+//         container.appendChild(myList);
+
+//         selectedItems.forEach(selectedItem => {
+
+//             const listItem = document.createElement('li');
+//             myList.appendChild(listItem);
+
+//             const getItemDisplay = document.createElement('div');
+//             getItemDisplay.classList.add('item-display');
+//             listItem.appendChild(getItemDisplay);
+
+//             getItemDisplay.textContent = `Item id: ${selectedItem.id} Name: ${selectedItem.name} Description: ${selectedItem.description} Quantity: ${selectedItem.quantity} Offer: ${selectedItem.offer_quantity} Category id: ${selectedItem.category_id}`;
+//         });
+//     } else {
+//         console.error(`No items found with category_id = ${name}.`);
+//     }
+// }
 
 async function getItems() {
     try {
@@ -111,6 +135,24 @@ async function fetchItems() {
         throw error; // Re-throw the error to be caught in the higher level
     }
 }
+
+async function fetchItemsBySearch(name) {
+    try {
+        const response = await fetch(`http://localhost:3000/items/search?str=${name}`);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('Data received:', data);
+        return data; // Return the data
+    } catch (error) {
+        console.error('Fetch error:', error);
+        throw error; // Re-throw the error to be caught in the higher level
+    }
+}
+
 
 getItemByCategoryId(3);
 
