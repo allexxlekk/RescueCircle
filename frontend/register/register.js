2fb040464678a3a2a2001d05236f0c79d512ec97
ntinoskost16 <ntinos.kostos@gmail.com>
2024-02-10 23:26:07 +0200

    create register page

--- frontend/register/register.js
+++ frontend/register/register.js
@@ -1,46 +1,68 @@
-document.addEventListener('DOMContentLoaded', () => {
+let mymap = L.map("mapid").setView([51.505, -0.09], 13);
+let theMarker = null;
 
-    const usernameInput = document.getElementById('username');
-    const usernameAvailabilityResult = document.getElementById('usernameAvailabilityResult');
+document.addEventListener("DOMContentLoaded", () => {
 
-    // Function to check username availability
-    const checkUsernameAvailability = async () => {
-        const username = usernameInput.value;
 
-        if (username === '')
-            usernameAvailabilityResult.textContent = '';
-        else
-            // Send a GET request to the server to check username availability
-            try {
-                const response = await fetch('http://localhost:3000/users/checkusername?str=' + encodeURIComponent(username));
-                const isAvailable = await response.json();
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution:
+            'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 18,
+    }).addTo(mymap);
+    mymap.on('click', onMapClick);
 
-                console.log(isAvailable)
-                if (isAvailable) {
-                    usernameAvailabilityResult.textContent = 'Username is available.';
-                } else {
-                    usernameAvailabilityResult.textContent = 'Username is already taken.';
-                }
-            } catch (error) {
-                console.error('Error checking username availability:', error);
-                usernameAvailabilityResult.textContent = 'Error checking username availability.';
-            }
-    };
-
-    // Define a debounce function with a 300ms delay
-    const debounceCheckUsernameAvailability = debounce(checkUsernameAvailability, 300);
-
-    // Add a click event listener to the "Check Username Availability" button
-    usernameInput.addEventListener('input', debounceCheckUsernameAvailability)
 });
 
-// Set a timer before 
-function debounce(func, delay) {
-    let timer;
-    return function (...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            func.apply(this, args);
-        }, delay);
-    };
+
+function onMapClick(e) {
+    // Check if the marker already exists
+    if (theMarker != null) {
+        // Move the existing marker to the new location
+        theMarker.setLatLng(e.latlng);
+    } else {
+        // Create a new draggable marker at the clicked position
+        theMarker = L.marker(e.latlng, { draggable: true }).addTo(mymap);
+
+        // Event listener for the marker to log new position after drag ends
+        theMarker.on('dragend', function (event) {
+            var marker = event.target;
+            var position = marker.getLatLng();
+            console.log("Marker new position: ", position);
+        });
+    }
+};
+
+//API CALLS
+
+async function postRegister(register) {
+    const postRegister = await fetch('http://localhost:3000/reqister', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(register),
+    });
 }
+
+// 
+
+let registerButton = document.getElementById('register-button');
+registerButton.addEventListener('click', async () => {
+
+    let position = theMarker.getLatLng();
+
+    console.log(username, fullname, email, password);
+    const registerObject = {
+        username: document.getElementById('username').value,
+        fullname: document.getElementById('fullname').value,
+        email: document.getElementById('email').value,
+        password: document.getElementById('password').value,
+        phone: document.getElementById('phone').value,
+        longitude: position.lng,
+        latitude: position.lat,
+    }
+    console.log(registerObject);
+    await postRegister(registerObject);
+});
+
+
