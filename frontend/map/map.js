bf4055677a2a055ec8543e50962d1179bb4499ec
ntinoskost16 <ntinos.kostos@gmail.com>
2024-02-12 20:46:03 +0200

    create the 5km radius circle area around the base

--- frontend/map/map.js
+++ frontend/map/map.js
@@ -1,7 +1,7 @@
-var mymap = L.map("mapid").setView([51.505, -0.09], 13);
+var mymap = L.map("mapid").setView([38.242, 21.727], 12);
 var theMarker = null;
 document.addEventListener("DOMContentLoaded", () => {
-  
+
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
@@ -9,24 +9,35 @@ document.addEventListener("DOMContentLoaded", () => {
     maxZoom: 18,
   }).addTo(mymap);
   mymap.on('click', onMapClick);
-  
+
 });
 
 
 function onMapClick(e) {
-    // Check if the marker already exists
-    if (theMarker != null) {
-      // Move the existing marker to the new location
-      theMarker.setLatLng(e.latlng);
-    } else {
-      // Create a new draggable marker at the clicked position
-      theMarker = L.marker(e.latlng, { draggable: true }).addTo(mymap);
-      
-      // Event listener for the marker to log new position after drag ends
-      theMarker.on('dragend', function(event) {
-        var marker = event.target;
-        var position = marker.getLatLng();
-        console.log("Marker new position: ", position);
-      });
-    }
+  // Check if the marker already exists
+  if (theMarker != null) {
+    // Move the existing marker to the new location
+    theMarker.setLatLng(e.latlng);
+  } else {
+    // Create a new draggable marker at the clicked position
+    theMarker = L.marker(e.latlng, { draggable: true }).addTo(mymap);
+
+    // Event listener for the marker to log new position after drag ends
+    theMarker.on('dragend', function (event) {
+      var marker = event.target;
+      var position = marker.getLatLng();
+      console.log("Marker new position: ", position);
+    });
+
+
+  }
 };
+
+let center = [38.242, 21.727];
+const radius = 5000;
+const circle = L.circle(center, {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: radius
+}).addTo(mymap);
