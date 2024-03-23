c6c2a0504cbb7f05149e9c13f069f72339cf50cc
allexxlekk <allexxlekk@gmail.com>
2024-03-23 11:52:24 +0200

    Add todos for backend, add rescuer services

--- backend/routes/rescuer.js
+++ backend/routes/rescuer.js
@@ -1,57 +1,57 @@
-const express = require('express');
-const rescuerService = require('../services/rescuerService.js');
+const express = require("express");
+const rescuerService = require("../services/rescuerService.js");
 const router = express.Router();
 
-
-//ADD NEW REQUEST
-router.post('/load', async (req, res) => {
-
+router.post("/load", async (req, res) => {
   try {
     const inventory = req.body;
     const inventoryLoaded = await rescuerService.loadInventory(inventory);
     if (inventoryLoaded) {
-      res.status(201).json({ message: 'Inventory loaded successfully' });
+      res.status(201).json({ message: "Inventory loaded successfully" });
     }
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: 'Error loading inventory' });
+    res.status(500).json({ error: "Error loading inventory" });
   }
 });
 
+router.get("/inventory", async (req, res) => {
+  try {
 
-//todo must work with list
-router.post('/unload', async (req, res) => {
+    const rescuerId = req.query.id;
+    const inventory = await rescuerService.fetchInventory(rescuerId);
+    res.status(200).json(inventory);
 
-    try {
-      const inventory = req.body;
-      const inventoryUnloaded = await rescuerService.unloadInventory(inventory);
-      if (inventoryUnloaded) {
-        res.status(201).json({ message: 'Inventory unloaded successfully' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error unloading inventory' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error fetching inventory" });
+  }
+});
+
+router.get("/unload", async (req, res) => {
+  try {
+    const rescuerId = req.query.id;
+    const inventoryUnloaded = await rescuerService.unloadInventory(rescuerId);
+    if (inventoryUnloaded) {
+      res.status(201).json({ message: "Inventory unloaded successfully" });
     }
-  });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error unloading inventory" });
+  }
+});
 
-  router.post('/deliver', async (req, res) => {
-
-    try {
-      const inventory = req.body;
-      const inventoryDelivered = await rescuerService.deliver(inventory);
-      if (inventoryDelivered) {
-        res.status(201).json({ message: 'Inventory delivered successfully' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Error delivered inventory' });
+router.post("/deliver", async (req, res) => {
+  try {
+    const inventory = req.body;
+    const inventoryDelivered = await rescuerService.deliver(inventory);
+    if (inventoryDelivered) {
+      res.status(201).json({ message: "Inventory delivered successfully" });
     }
-  });
-
-  
-
-
-
-
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Error delivered inventory" });
+  }
+});
 
 module.exports = router;
--- backend/services/rescuerService.js
+++ backend/services/rescuerService.js
@@ -1,26 +1,5 @@
 const dbConnection = require("../config/db");
 
-const addRequest = async (requestData) => {
-  try {
-    const query = `
-  INSERT INTO request (item_id, number_of_people, citizen_id, quantity)
-  VALUES (?, ?, ?, (SELECT offer_quantity FROM item WHERE id = ?) * ?)
-`;
-    const values = [
-      requestData.itemId,
-      requestData.numberOfPeople,
-      requestData.citizenId,
-      requestData.itemId,
-      requestData.numberOfPeople,
-    ];
-    await dbConnection.promise().query(query, values);
-    return true; // Request added successfully
-  } catch (err) {
-    console.error("Error on add category:", err);
-    throw err;
-  }
-};
-
 const loadInventory = async (inventory) => {
   try {
     // Check if there is enough quantity available for the item
@@ -64,44 +43,50 @@ const loadInventory = async (inventory) => {
   }
 };
 
-const unloadInventory = async (inventory) => {
+const unloadInventory = async (rescuerId) => {
   try {
     // Check if there is enough quantity available in rescuer's inventory
     const rescuerInventoryQuery =
-      "SELECT amount FROM rescuer_inventory WHERE item_id = ? AND rescuer_id = ?";
-    const [rescuerInventoryRows] = await dbConnection
+      "SELECT amount, item_id as itemId FROM rescuer_inventory WHERE rescuer_id = ?";
+    const [rescuerInventory] = await dbConnection
       .promise()
-      .query(rescuerInventoryQuery, [inventory.itemId, inventory.rescuerId]);
-    if (rescuerInventoryRows.length === 0) {
-      throw new Error("Rescuer does not have this item in inventory");
-    }
-    const rescuerAmount = rescuerInventoryRows[0].amount;
-    if (rescuerAmount < inventory.amount) {
-      throw new Error("Insufficient quantity in rescuer's inventory");
-    }
+      .query(rescuerInventoryQuery, [rescuerId]);
 
     // Proceed with unloading inventory
     const unloadQuery = `
-        UPDATE rescuer_inventory SET amount = amount - ? WHERE item_id = ? AND rescuer_id = ?;
+        UPDATE rescuer_inventory SET amount = 0 WHERE rescuer_id = ?;
       `;
-    await dbConnection
-      .promise()
-      .query(unloadQuery, [
-        inventory.amount,
-        inventory.itemId,
-        inventory.rescuerId,
-      ]);
+    await dbConnection.promise().query(unloadQuery, [rescuerId]);
 
-    // Update the quantity of the item
     const updateItemQuery =
       "UPDATE item SET quantity = quantity + ? WHERE id = ?";
-    await dbConnection
-      .promise()
-      .query(updateItemQuery, [inventory.amount, inventory.itemId]);
+    rescuerInventory.forEach(async (element) => {
+      await dbConnection
+        .promise()
+        .query(updateItemQuery, [element.amount, element.itemId]);
+    });
+    // Update the quantity of the item
 
     return true; // Inventory unloaded successfully
   } catch (err) {
     console.error("Error on unload inventory:", err);
+    throw err;
+  }
+};
+
+const fetchInventory = async () => {
+  try {
+    const query = `
+            SELECT ri.amount, item.name, item_category.name AS category_name
+            FROM rescuer_inventory AS ri
+                    INNER JOIN item ON ri.item_id = item.id
+                    INNER JOIN item_category ON item.category_id = item_category.id
+                    WHERE ri.amount > 0;
+        `;
+    const [result] = await dbConnection.promise().query(query);
+    return result;
+  } catch (err) {
+    console.error("Error get inventory:", err);
     throw err;
   }
 };
@@ -222,10 +207,8 @@ const fetchCitizenRequests = async () => {
 };
 
 module.exports = {
-  addRequest,
-  fetchRequestsForCitizen,
-  fetchCitizenRequests,
   loadInventory,
   unloadInventory,
   deliver,
+  fetchInventory,
 };
