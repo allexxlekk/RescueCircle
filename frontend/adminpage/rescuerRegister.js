9adcd5e11c76cb6ad164c7126e9b60d3d3b7ec95
ntinoskost800 <ntinos_kost@digipath.gr>
2024-07-30 19:47:17 +0300

    Create rescuer management page, create register rescuer form

--- frontend/adminpage/rescuerRegister.js
+++ frontend/adminpage/rescuerRegister.js
@@ -1,42 +1,51 @@
 async function postRegister(register) {
-    const postRegister = await fetch("http://localhost:3000/register", {
+    const response = await fetch("http://localhost:3000/admin/rescuer-management/rescuers", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(register),
     });
+
+    if (!response.ok) {
+        throw new Error("Error registering rescuer");
+    }
+
+    return await response.json();
 }
 
-//
-document.addEventListener("DOMContentLoaded", async () => {
-
+document.addEventListener("DOMContentLoaded", () => {
     const registerButton = document.getElementById("register-button");
     registerButton.addEventListener('click', registerUser);
 });
 
-const registerUser = async () => {
+const registerUser = async (event) => {
+    event.preventDefault(); // Prevent form submission
 
     const registerObject = {
         username: document.getElementById("username").value,
-        fullname: document.getElementById("fullname").value,
-        role: 'RESCUER',
-        latitude: '0',
-        longitude: '0',
+        name: document.getElementById("fullname").value,
+        // role: 'RESCUER',
         email: document.getElementById("email").value,
         password: document.getElementById("password").value,
         phone: document.getElementById("phone").value,
         vehicleType: document.getElementById("vehicle_type").value,
     };
-    console.log(registerObject);
-    await postRegister(registerObject);
-    alert("Registration Confirmed!");
+
+    try {
+        console.log(registerObject);
+        const response = await postRegister(registerObject);
+        alert("Registration Confirmed!");
+        console.log("Response from server:", response);
+    } catch (error) {
+        console.error("Error:", error);
+        alert("Error registering rescuer. Please try again.");
+    }
 };
 
 async function getBaseLocation() {
     try {
         const response = await fetch("http://localhost:3000/baseLocation");
-
         return await response.json(); // Return the data
     } catch (error) {
         console.error("Fetch error:", error);
