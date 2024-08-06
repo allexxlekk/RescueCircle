47b4bcd737c8dac7aadcab9895f627f7cbdec733
ntinoskost800 <ntinos_kost@digipath.gr>
2024-08-06 20:24:12 +0300

    Logout button added in every page in admin. Modal clickoutside to close in announcements adminPage

--- frontend/admin/announcement-management/announcement-management.js
+++ frontend/admin/announcement-management/announcement-management.js
@@ -18,6 +18,20 @@ document.addEventListener('DOMContentLoaded', async () => {
     console.log(announcements);
     showAnnouncements(announcements);
     updateAnnouncementCount();
+
+    const modal = document.getElementById("announcementModal");
+    const span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+    // When the user clicks anywhere outside of the modal, close it
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
+    }
 });
 
 async function fetchItems() {
