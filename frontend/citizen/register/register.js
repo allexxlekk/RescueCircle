c731d66bc829adb6318f3c4ad4b83e72f795332c
alexlek <allexxlekk@gmail.com>
2024-08-25 11:17:20 +0300

    fix map

--- frontend/citizen/register/register.js
+++ frontend/citizen/register/register.js
@@ -1,6 +1,6 @@
 import apiUtils from "../../utils/apiUtils.mjs";
 
-let mymap = L.map("mapid").setView([51.505, -0.09], 13);
+let mymap = L.map("map").setView([51.505, -0.09], 13);
 let theMarker = null;
 
 document.addEventListener("DOMContentLoaded", async () => {
